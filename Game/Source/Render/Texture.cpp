24df06e63c452917cdc11577d0144adb091c05f0
djordjetane <djordje.tanaskovic1998@gmail.com>
2021-02-11 23:52:08 +0100

    Added TextureType and simplified creation
    TextureType as enum class, will be integrated in Model
    Main functionality now in one method
    Fixed unnecessary object making
    Removed exceptions

--- Game/Source/Render/Texture.cpp
+++ Game/Source/Render/Texture.cpp
@@ -4,55 +4,10 @@
 
 #include <stb_image.h>
 
-Render::Texture::Texture(std::string name, const std::string& type)
+Render::Texture::Texture(std::string name, Render::TextureType type)
 	: m_Name(name.substr( name.find_last_of('/') + 1)), m_Type(type)
 {	
-	glGenTextures(1, &m_ID);
-	glBindTexture(GL_TEXTURE_2D, m_ID);
-
-	
-
-
-	// Calculating full texture path
-	// name.insert(0, textureDir);
-
-	int width, height, nrChannels;
-	
-	unsigned char* data = stbi_load(name.c_str(), &width, &height, &nrChannels, 0);
-
-	if (data)
-	{
-		GLenum format = GL_RED;
-		if (nrChannels == 1)
-			format = GL_RED;
-		else if (nrChannels == 3)
-			format = GL_RGB;
-		else if (nrChannels == 4)
-			format = GL_RGBA;
-
-		glBindTexture(GL_TEXTURE_2D, m_ID);
-		glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
-		glGenerateMipmap(GL_TEXTURE_2D);
-
-
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	}
-	else
-	{
-		LOG_ERROR("Failed to load texture\nTexture: {}", name);
-		stbi_image_free(data);
-		throw std::exception("Failed to load image");
-	}
-
-	
-#ifdef _DEBUG
-	LOG_INFO("Loaded texture {}", m_Name);
-#endif // DEBUG	
-
-	stbi_image_free(data);
+	m_ID = LoadNativeTexture(name, type);
 }
 
 unsigned Render::Texture::GetID()
@@ -65,8 +20,55 @@ std::string Render::Texture::GetName()
 	return m_Name;
 }
 
-std::string Render::Texture::GetType()
+Render::TextureType Render::Texture::GetType()
 {
 	return m_Type;
+}
+
+unsigned Render::Texture::LoadNativeTexture(std::string name, Render::TextureType type)
+{
+	unsigned id;
+
+	glGenTextures(1, &id);
+	glBindTexture(GL_TEXTURE_2D, id);
+	
+
+	int width, height, nrChannels;
+
+	unsigned char* data = stbi_load(name.c_str(), &width, &height, &nrChannels, 0);
+
+	if (data)
+	{
+		GLenum format = GL_RED;
+		if (nrChannels == 1)
+			format = GL_RED;
+		else if (nrChannels == 3)
+			format = GL_RGB;
+		else if (nrChannels == 4)
+			format = GL_RGBA;
+
+		glBindTexture(GL_TEXTURE_2D, id);
+		glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+
+
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+		LOG_INFO("Loaded {} {}", type, name);
+	}
+	else
+	{
+		LOG_ERROR("Failed to load texture\nTexture: {}", name);
+		id = 0;
+	}
+
+
+
+	stbi_image_free(data);
+
+	return id;
 }
 
--- Game/Source/Render/Texture.h
+++ Game/Source/Render/Texture.h
@@ -4,23 +4,64 @@
 
 namespace Render {	
 
+	
 	class Texture
 	{
 	public:	
 
-		Texture(std::string name, const std::string& type = "texture_diffuse");
+		Texture(std::string name, TextureType type = TextureType::DIFFUSE);
 
 		unsigned GetID();
-		std::string GetName();
-		std::string GetType();
+		std::string GetName();		
+		Render::TextureType GetType();
 		
-		// inline static const char* textureDir = "Resources\\Textures\\";
+		static unsigned LoadNativeTexture(std::string name
+					, Render::TextureType type = Render::TextureType::DIFFUSE);	
+
+		// string in format "texture_{type}"
+		inline static TextureType TypeFromString(const std::string& type_)
+		{
+			return (TextureType)std::toupper(type_[8]);
+		}
+
+		inline static std::string TypeToString(TextureType type)
+		{
+			switch (type)
+			{
+			case Render::TextureType::DIFFUSE:
+				return std::string{ "texture_diffuse" };
+			case Render::TextureType::NORMAL:
+				return std::string{ "texture_normal" };
+			case Render::TextureType::SPECULAR:
+				return std::string{ "texture_specular" };
+			case Render::TextureType::HEIGHT:
+				return std::string{ "texture_height" };
+			case Render::TextureType::TEX_ERRORTYPE:				
+			default:
+				break;
+			}
+
+			return std::string{ "ErrorTextureType" };
+		}
+
+		/* string in format "texture_{_type_}N" where N is 
+		*  ordinary number of texture 
+		*/
+		inline static std::string TypeToStringN(TextureType type, unsigned N)
+		{
+			return TypeToString(type) + std::to_string(N);
+		}
 
 	private:
 		unsigned m_ID;
-		std::string m_Name;
-		std::string m_Type;
-		
+		std::string m_Name;		
+		TextureType m_Type;		
 	};
-
+	
 } // Redner
+
+
+#define TDIFFUSE   Render::TextureType::DIFFUSE
+#define TNORMAL    Render::TextureType::NORMAL
+#define TSPECULAR  Render::TextureType::SPECULAR
+#define THEIGHT    Render::TextureType::HEIGHT
