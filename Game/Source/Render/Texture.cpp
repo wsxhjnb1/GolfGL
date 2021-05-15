79d41450dcd9e746ed2fac3d668134eeef5ac777
djordjetane <djordje.tanaskovic1998@gmail.com>
2021-05-15 11:05:51 +0200

    Texture fixses

--- Game/Source/Render/Texture.cpp
+++ Game/Source/Render/Texture.cpp
@@ -2,132 +2,152 @@
 
 #include "Texture.h"
 
-Render::Texture::Texture(const std::string &name, Render::TextureType type)
-    : m_Name(name.substr(name.find_last_of('/') + 1)), m_Type(type)
-{
-    if (m_Name.empty())
-    {
-        m_Name = name;
-        m_ID   = LoadNativeTexture(std::string{"Resources/Textures/"}.append(name));
-    }
 
-    m_ID = LoadNativeTexture(name);
+
+Render::Texture::Texture(const std::string& name, Render::TextureType type, GLenum target)
+	: m_name(name.substr( name.find_last_of('/') + 1)), m_type(type)
+{
+    int channels = 0;
+    if(type == TNORMAL || type == THEIGHT)
+        channels = 1;
+
+    if ( m_name.empty() )
+    {
+        m_name = name;
+        m_ID = LoadNativeTexture( std::string{ "Resources/Textures/" }.append( name ), target, channels );
+    }
+    else
+	    m_ID = LoadNativeTexture(name, target, channels);
 }
 
-unsigned Render::Texture::GetID() { return m_ID; }
+unsigned Render::Texture::GetID() const
+{
+	return m_ID;
+}
 
-std::string Render::Texture::GetName() { return m_Name; }
+std::string Render::Texture::GetName() const
+{
+	return m_name;
+}
 
-Render::TextureType Render::Texture::GetType() { return m_Type; }
+Render::TextureType Render::Texture::GetType() const
+{
+	return m_type;
+}
 
-unsigned Render::Texture::GenID(GLenum target)
+
+unsigned Render::Texture::GenID( GLenum target )
 {
     unsigned id;
-    glGenTextures(1, &id);
-    glBindTexture(target, id);
+    glGenTextures( 1, &id );
+    glBindTexture( target, id );
 
     return id;
 }
 
-Render::RawImage Render::Texture::LoadRawImage(const std::string &name, int desiredChannels)
-{
-    int width, height, nChannels; // NOLINT
+Render::RawImage Render::Texture::LoadRawImage( const std::string &name, int desiredChannels )
+{        
+    int width, height, nChannels;
+    
+    unsigned char *data = stbi_load( name.c_str(), &width, &height, &nChannels, desiredChannels );	
 
-    unsigned char *data = stbi_load(name.c_str(), &width, &height, &nChannels, desiredChannels);
-
-    return {width, height, nChannels, data};
+	return { width, height, nChannels, data };
 }
 
-GLint Render::Texture::GetFormat(int nChannels)
-{
-    switch (nChannels)
+GLint Render::Texture::GetFormat( int nChannels )
+{    
+	switch ( nChannels )
     {
-    case 2: return GL_RG;
-    case 3: return GL_RGB;
-    case 4: return GL_RGBA;
-    default: return GL_RED;
+    case 2:
+        return GL_RG;
+    case 3:
+        return GL_RGB;
+    case 4:
+        return GL_RGBA;
+    default:
+        return GL_RED;
     }
 }
 
-void Render::Texture::BindTextureImage(GLenum target, unsigned id, RawImage &rawimage, bool genmm)
+void Render::Texture::BindTextureImage( unsigned id, RawImage& rawimage, GLenum target, bool genmm )
 {
-    auto [width, height, nChannels, data] = rawimage.Get();
+    auto [width, height, nChannels, data] = std::move( rawimage.Get() );
+    
+    auto iForm = GetFormat( nChannels );
 
-    auto iForm = GetFormat(nChannels);
-
-    BindTextureImage(target, id, width, height, iForm, data, genmm);
+	BindTextureImage( id, width, height, iForm, data, target, genmm );
 }
 
-void Render::Texture::BindTextureImage(GLenum target,
-                                       unsigned id,
-                                       int width,
-                                       int height,
-                                       GLint internalformat,
-                                       const stbi_uc *data,
-                                       bool genmm,
-                                       GLenum format,
-                                       GLint level,
-                                       GLenum type)
+void Render::Texture::BindTextureImage( unsigned id, int width, int height, GLint internalformat,
+                                        const stbi_uc *data, GLenum target, bool genmm, GLenum format, GLint level, GLenum type )
 {
-    glBindTexture(GL_TEXTURE_2D, id);
-    glTexImage2D(GL_TEXTURE_2D, level, internalformat, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+    format = format != 0 ? format : internalformat;
+    glBindTexture( target, id );
+    glTexImage2D( target, level, internalformat, width, height, 0, format, type, data );
     if (genmm)
-    {
-        glGenerateMipmap(GL_TEXTURE_2D);
-    }
-}
-
-template <>
-inline void Render::Texture::SetTexParam(GLenum target, GLenum pname, float param)
-{
-    glTexParameterf(target, pname, param);
-}
-
-template <>
-inline void Render::Texture::SetTexParam(GLenum target, GLenum pname, float *param)
-{
-    glTexParameterfv(target, pname, param);
-}
-
-unsigned Render::Texture::LoadNativeTexture(const std::string &name, GLenum target)
-{
-    auto id = GenID(target);
-    RawImage rawIm{LoadRawImage(name)};
-
-    if (rawIm.data == nullptr)
-    {
-        LOG_ERROR("Failed to load texture\nTexture: {}", name);
-
-#ifdef _DEBUG
-        LOG_ERROR("Error code: {}", glGetError());
-#endif // _DEBUG
-
-        return 0;
-    }
-
-    BindTextureImage(target, id, rawIm);
-
-    SetTexParam(target, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    SetTexParam(target, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    SetTexParam(target, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    SetTexParam(target, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-    LOG_INFO("Loaded texture {}", name);
-
-    return id;
+		glGenerateMipmap( target );
 }
 
 std::string Render::Texture::TypeToString(TextureType type)
 {
     switch (type)
     {
-    case Render::TextureType::DIFFUSE: return std::string{"texture_diffuse"};
-    case Render::TextureType::NORMAL: return std::string{"texture_normal"};
-    case Render::TextureType::SPECULAR: return std::string{"texture_specular"};
-    case Render::TextureType::HEIGHT: return std::string{"texture_height"};
-    case Render::TextureType::TEX_ERRORTYPE:
-    default: break;
+    case TextureType::DIFFUSE:
+        return std::string{"texture_diffuse"};
+    case TextureType::NORMAL:
+        return std::string{"texture_normal"};
+    case TextureType::SPECULAR:
+        return std::string{"texture_specular"};
+    case TextureType::HEIGHT:
+        return std::string{"texture_height"};
+    case TextureType::TEX_ERRORTYPE: default:
+        break;
     }
 
     return std::string{"ErrorTextureType"};
 }
+
+template<typename T>
+void Render::Texture::SetTexParam( GLenum pname, T param, GLenum target )
+{
+    glTexParameterf( target, pname, param );
+}
+
+template<typename T>
+void Render::Texture::SetTexParam( GLenum pname, const T* param, GLenum target )
+{
+    glTexParameterfv( target, pname, param );
+}
+
+unsigned Render::Texture::LoadNativeTexture( const std::string &name, GLenum target, int desiredChannels)
+{		
+    auto id = GenID( target );
+	RawImage rawIm{ LoadRawImage( name, desiredChannels ) };
+	
+
+	if ( rawIm.data == nullptr )
+    {
+        LOG_ERROR( "Failed to load texture\nTexture: {}", name );
+
+#ifdef _DEBUG
+        LOG_ERROR( "Error code: {}", glGetError() );
+#endif // _DEBUG
+        
+        return 0;
+    }
+
+    BindTextureImage( id, rawIm, target );
+
+    SetTexParam( GL_TEXTURE_MIN_FILTER, GL_LINEAR, target );
+    SetTexParam( GL_TEXTURE_MAG_FILTER, GL_LINEAR, target );
+	SetTexParam( GL_TEXTURE_WRAP_S,     GL_REPEAT, target );
+    SetTexParam( GL_TEXTURE_WRAP_T,     GL_REPEAT, target );    
+
+	LOG_INFO("Loaded texture {}", name);
+    glBindTexture(target, 0);
+
+	return id;
+}
+
+
+
--- Game/Source/Render/Texture.h
+++ Game/Source/Render/Texture.h
@@ -2,117 +2,114 @@
 
 #include <string>
 
-namespace Render
-{
-    enum class TextureType
+
+
+namespace Render {	
+
+	enum class TextureType
     {
         DIFFUSE = 0,
         NORMAL,
         SPECULAR,
         HEIGHT,
         TEX_ERRORTYPE
-    };
+    };		
 
-    struct RawImage
-    {
+
+	struct RawImage
+    {        
         int width, height, nChannels;
-        stbi_uc *data = nullptr;
+        stbi_uc* data = nullptr;
 
-        using Properties = std::tuple<int, int, int, stbi_uc *>;
-
-        inline Properties Get()
+		using Properties = std::tuple<int, int, int, stbi_uc*>;
+        Properties Get()
         {
-            return {width, height, nChannels, data};
+            return { width, height, nChannels, data };
         }
 
-        inline stbi_uc *Set(const char *path)
+		stbi_uc* Set( const char *path, int desiredChannels = 0 )
         {
-            stbi_image_free(data);
-            data = stbi_load(path, &width, &height, &nChannels, 0);
+            stbi_image_free( data );
+            data = stbi_load( path, &width, &height, &nChannels, desiredChannels);
             return data;
         }
-
-        ~RawImage()
-        {
-            stbi_image_free(data);
+		
+		~RawImage()
+        {            
+            stbi_image_free( data );
         }
     };
 
-    class Texture
-    {
-    public:
-        Texture(const std::string &name, TextureType type = TextureType::DIFFUSE);
+	class Texture
+	{
+	public:	
 
-        unsigned GetID();
-        std::string GetName();
-        Render::TextureType GetType();
+		Texture(const std::string& name, TextureType type = TextureType::DIFFUSE, GLenum target = GL_TEXTURE_2D);
 
-        /* Static texture loading functions */
-        // -------------------------------------
+		unsigned GetID() const;
+		std::string GetName() const;		
+		TextureType GetType() const;
+		
+
+
+		/* Static texture loading functions */
+		// -------------------------------------
         /* Load texture and returns texture ID */
-        static unsigned LoadNativeTexture(const std::string &name, GLenum target = GL_TEXTURE_2D);
+		static unsigned LoadNativeTexture(const std::string& name, GLenum target = GL_TEXTURE_2D, int desiredChannels = 0 );
 
-        /* Generates ID for texture target */
-        static unsigned GenID(GLenum target = GL_TEXTURE_2D);
+		/* Generates ID for texture target */
+        static unsigned GenID( GLenum target = GL_TEXTURE_2D );
+		
+		/* Stores raw image properties in RAII struct */				
+		static RawImage LoadRawImage( const std::string &name, int desiredChannels = 0 );
 
-        /* Stores image properties in RAII struct */
-        static RawImage LoadRawImage(const std::string &name, int desiredChannels = 0);
+		/* Converts number of channels to native enum format */
+		static GLint GetFormat( int nChannels );
 
-        /* Converts number of channels to native enum format */
-        static GLint GetFormat(int nChannels);
+		/* Binds to array and specifies texture image to native lib */
+        static void BindTextureImage( unsigned id, RawImage &rawimage, GLenum target = GL_TEXTURE_2D, bool genmm = true );
+        static void BindTextureImage( unsigned id, int width, int height, GLint internalformat,
+                                      const stbi_uc *data, GLenum target = GL_TEXTURE_2D, bool genmm = true, GLenum format = 0, 
+									  GLint level = 0, GLenum type = GL_UNSIGNED_BYTE );
 
-        /* Binds to array and specifies texture image to native lib */
-        static void BindTextureImage(GLenum target, unsigned id, RawImage &rawimage, bool genmm = true);
-        static void BindTextureImage(GLenum target, unsigned id, int width, int height, GLint internalformat,
-                                     const stbi_uc *data, bool genmm = true, GLenum format = GL_RGB, GLint level = 0,
-                                     GLenum type = GL_UNSIGNED_BYTE);
-
-        /* Set texture parameter */
-        template <typename T = int>
-        static void SetTexParam(GLenum target, GLenum pname, T param);
+		/* Set texture parameter */
+		template<typename T>
+        static void SetTexParam( GLenum pname,		 T param,  GLenum target = GL_TEXTURE_2D );
         /* Set texture parameters */
-        template <typename T = int>
-        static void SetTexParam(GLenum target, GLenum pname, const T *param);
+        template <typename T> 
+		static void SetTexParam( GLenum pname, const T* param, GLenum target = GL_TEXTURE_2D );
 
-        /* Texture type parsers */
+
+		/* Texture type parsers */
         // -------------------------------------
-        /* str_ must be in format: texture_{type} */
-        inline static TextureType TypeFromString(const std::string &str_)
-        {
-            return (TextureType)std::toupper(str_[8]);
-        }
+		/* str_ must be in format: texture_{type} */
+		inline static TextureType TypeFromString(const std::string& str_)
+		{
+            return static_cast<TextureType>(std::toupper(str_[8]));
+		}
 
-        /* Returns formated string: texture_{type} */
+		/* Returns formated string: texture_{type} */
         static std::string TypeToString(TextureType type);
 
-        /* string in format: texture_{_type_}N
-           where N is ordinary number of texture */
-        inline static std::string TypeToStringN(TextureType type, unsigned N)
-        {
-            return TypeToString(type) + std::to_string(N);
-        }
+		/* string in format: texture_{_type_}N
+		   where N is ordinary number of texture */				  
+		inline static std::string TypeToStringN(TextureType type, unsigned N)
+		{
+			return TypeToString(type) + std::to_string(N);
+		}
+		
 
-    private:
-        unsigned m_ID;
-        std::string m_Name;
-        TextureType m_Type;
-    };
+	private:
+		unsigned m_ID;
+		std::string m_name;		
+		TextureType m_type;
+	};
 
-    template <typename T>
-    inline void Texture::SetTexParam(GLenum target, GLenum pname, T param)
-    {
-        glTexParameteri(target, pname, param);
-    }
 
-    template <typename T>
-    inline void Texture::SetTexParam(GLenum target, GLenum pname, const T *param)
-    {
-        glTexParameteriv(target, pname, param);
-    }
-} // namespace Render
+} // Redner
 
-// Aliases
-#define TDIFFUSE Render::TextureType::DIFFUSE // NOLINT
-#define TNORMAL Render::TextureType::NORMAL
-#define TSPECULAR Render::TextureType::SPECULAR
-#define THEIGHT Render::TextureType::HEIGHT
+
+#define TDIFFUSE   Render::TextureType::DIFFUSE
+#define TNORMAL    Render::TextureType::NORMAL
+#define TSPECULAR  Render::TextureType::SPECULAR
+#define THEIGHT    Render::TextureType::HEIGHT
