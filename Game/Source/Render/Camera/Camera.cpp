3f14b4d4cdccf6a608828164e41eb4a294de5095
djordjetane <djordje.tanaskovic1998@gmail.com>
2021-05-15 11:08:20 +0200

    Refs instead of copies

--- Game/Source/Render/Camera/Camera.cpp
+++ Game/Source/Render/Camera/Camera.cpp
@@ -2,25 +2,55 @@
 
 #include "Camera.h"
 
-void Camera::Init() { ms_Camera = std::move(std::unique_ptr<Camera>()); }
+void Camera::Init()
+{    
+    ms_Camera = std::move( std::unique_ptr<Camera>() );
+}
 
-void Camera::LookUp() { cameraPos += cameraSpeed * cameraFront; }
+void Camera::LookUp()
+{
+    cameraPos += cameraSpeed * cameraFront;
+}
 
-void Camera::LookDown() { cameraPos -= cameraSpeed * cameraFront; }
+void Camera::LookDown()
+{
+    cameraPos -= cameraSpeed * cameraFront;
+}
 
-void Camera::LookRight() { cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed; }
+void Camera::LookRight()
+{
+    cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+}
 
-void Camera::LookLeft() { cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed; }
+void Camera::LookLeft()
+{
+    cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+}
 
-glm::mat4 Camera::LookAt() { return glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp); }
+glm::mat4 Camera::LookAt()
+{
+    return glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+}
 
-void Camera::SetCameraSpeed(float dt) { cameraSpeed = MovementSpeed * dt; }
+void Camera::SetCameraSpeed(float dt)
+{
+    cameraSpeed = MovementSpeed * dt;
+}
 
-glm::vec3 Camera::GetCameraPos() { return cameraPos; }
+const glm::vec3& Camera::GetCameraPos()
+{
+    return cameraPos;
+}
 
-float Camera::GetCameraZoom() { return Zoom; }
+float Camera::GetCameraZoom()
+{
+    return Zoom;
+}
 
-glm::vec3 Camera::GetCameraFront() { return cameraFront; }
+const glm::vec3& Camera::GetCameraFront()
+{
+    return cameraFront;
+}
 
 void Camera::ProcessMouseMovement(float xoffset, float yoffset)
 {
@@ -30,7 +60,7 @@ void Camera::ProcessMouseMovement(float xoffset, float yoffset)
     Yaw += xoffset;
     Pitch += yoffset;
 
-    m_BoundAngleInRange(Pitch, -constrainPitchBound, constrainPitchBound);
+    m_BoundAngleInRange(Pitch, -constrainPitchBound, constrainPitchBound);    
 
     // update Front, Right and Up Vectors using the updated Euler angles
     m_UpdateCameraVectors();
@@ -42,13 +72,14 @@ void Camera::ProcessMouseScroll(float yoffset)
     m_BoundAngleInRange(Zoom, zoomLowerBound, zoomUpperBound);
 }
 
+
 void Camera::m_UpdateCameraVectors()
 {
     // calculate the new Front vector
     glm::vec3 front;
-    front.x     = cos(glm::radians(Yaw)) * cos(glm::radians(Pitch));
-    front.y     = sin(glm::radians(Pitch));
-    front.z     = sin(glm::radians(Yaw)) * cos(glm::radians(Pitch));
+    front.x = cos(glm::radians(Yaw)) * cos(glm::radians(Pitch));
+    front.y = sin(glm::radians(Pitch));
+    front.z = sin(glm::radians(Yaw)) * cos(glm::radians(Pitch));
     cameraFront = glm::normalize(front);
     // also re-calculate the Right and Up vector
     // LookRight();
--- Game/Source/Render/Camera/Camera.h
+++ Game/Source/Render/Camera/Camera.h
@@ -5,60 +5,58 @@
 class Camera
 {
 public:
-    Camera(const Camera &) = delete;
-    void operator=(const Camera &) = delete;
+	Camera(const Camera&)  = delete;   	
+	void operator=(const Camera&) = delete;
+	
+	static void Init();
+	inline static Camera& GetCamera() { return *ms_Camera; }
 
-    static void Init();
-
-    inline static Camera &GetCamera()
-    {
-        return *ms_Camera;
-    }
-
-    void LookUp();
-    void LookDown();
-    void LookRight();
-    void LookLeft();
+	void LookUp();
+	void LookDown();
+	void LookRight();
+	void LookLeft();
     glm::mat4 LookAt();
-    void SetCameraSpeed(float dt);
+	void SetCameraSpeed(float dt);
 
-    glm::vec3 GetCameraPos();
-    float GetCameraZoom();
+	const glm::vec3& GetCameraPos();
+	float GetCameraZoom();
 
-    glm::vec3 GetCameraFront();
+	const glm::vec3& GetCameraFront();
 
-    void ProcessMouseMovement(float xoffset, float yoffset);
-    void ProcessMouseScroll(float yoffset);
+
+	void ProcessMouseMovement(float xoffset, float yoffset);
+	void ProcessMouseScroll(float yoffset);
 
 private:
-    inline static std::unique_ptr<Camera> ms_Camera;
 
-    inline static glm::vec3 cameraPos{0.f, 0.f, 3.f};
-    inline static glm::vec3 cameraFront{0.f, 0.f, -1.f};
-    inline static glm::vec3 cameraUp{0.f, 1.f, 0.f};
-    inline static float cameraSpeed = 0.5f;
+	inline static std::unique_ptr<Camera> ms_Camera;	
 
-    inline static float Yaw = -90.f;
-    inline static float Pitch = 0.f;
-    inline static float MovementSpeed = 4.5f;
-    inline static float MouseSensitivity = 0.1f;
-    inline static float Zoom = 45.f;
+	inline static glm::vec3 cameraPos {0.f, 0.f, 3.f};
+	inline static glm::vec3 cameraFront { 0.f, 0.f, -1.f };
+	inline static glm::vec3 cameraUp{ 0.f, 1.f, 0.f };
+	inline static float cameraSpeed = 0.5f;
 
-    /* Bounds */
-    inline static const float constrainPitchBound = 89.f;
-    inline static const float zoomLowerBound = 1.f;
-    inline static const float zoomUpperBound = 45.f;
+	inline static float Yaw = -90.f;
+	inline static float Pitch = 0.f;		
+	inline static float MovementSpeed = 4.5f;
+	inline static float MouseSensitivity = 0.1f;
+	inline static float Zoom = 45.f;
 
-    Camera() = default;
+	/* Bounds */
+	inline static const float constrainPitchBound = 89.f;
+	inline static const float zoomLowerBound = 1.f;
+	inline static const float zoomUpperBound = 45.f;
+	
+	Camera() = default;
 
-    void m_UpdateCameraVectors();
+	void m_UpdateCameraVectors();
 
-    /* Keeping angles in range [lower, upper], to prevent flipping */
-    inline void m_BoundAngleInRange(float &angle, float lower, float upper)
+	/* Keeping angles in range [lower, upper], to prevent flipping */
+	inline void m_BoundAngleInRange(float& angle, float lower, float upper) const
     {
-        angle = std::max(lower, angle);
-        angle = std::min(angle, upper);
-    }
+		angle = std::max(lower, angle);
+		angle = std::min(angle, upper);
+	}
 };
 
 #define CAMERA Camera::GetCamera()
