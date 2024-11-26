581de2e44fadfbb182058ed98f45adebbd8bc7d3
wsxhjnb1 <1057334122xie@gmail.com>
2024-11-26 04:51:52 -0500

    fix warning

--- Game/Source/Data/WindowData.h
+++ Game/Source/Data/WindowData.h
@@ -4,8 +4,8 @@ struct WindowData
 {
     inline static int width = 1280;
     inline static int height = 720;
-    inline static constexpr char *windowTitle = "GolfGL";
-    inline static constexpr char *glslVersion = "#version 330";
+    inline static constexpr const char *windowTitle = "GolfGL";
+    inline static constexpr const char *glslVersion = "#version 330";
     inline static bool fxaa = true;
     inline static bool vsync = true;
 
